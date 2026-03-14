428e72d17cc18fec20eed55a2008644f269dc479
Hansanto <anthony.hanson@outlook.fr>
2026-03-14 18:55:13 +0100

    chore: format

--- test/playwright/counter.spec.ts
+++ test/playwright/counter.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test.describe('Counter', { tag: ['@responsive', '@dark'] }, () => {
-    test('counter increments', async ({page}) => {
+    test('counter increments', async ({ page }) => {
         await page.goto('/');
         await expect(page).toHaveScreenshot('counter-initial.png');
         const button = page.getByRole('button');
